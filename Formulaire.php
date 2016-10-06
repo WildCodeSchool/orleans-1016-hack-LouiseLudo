334ef32f51623ffe6505cc36c978d8123415ce8d
louisemunier <munier.louise0@gmail.com>
2016-10-06 15:55:29 +0200

    Created formulaire, formulaire.css, deezerAPI

---
+++ Formulaire.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<form methode="post" action="">
+    <label for="ville">Votre ville : <br></label>
+    <input type="text" name="ville" placeholder="Tapper votre ville de résidence ici" class="input_ville"/>
+    <input type="submit" value="Envoyer"/>
+</form>
--- index.php
+++ index.php
@@ -5,9 +5,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+    <link rel="stylesheet" type="text/css" href="css/stylesheet_formulaire.css">
     <title>WEATHERMOOD</title>
+
+    <script src="http://cdn-files.deezer.com/js/min/dz.js"></script>
+    <script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
+
 </head>
 <body>
+
+    <?php include ('deezerAPI.php') ?>
+    <?php include ('Formulaire.php') ?>
 
 </body>
 </html>
