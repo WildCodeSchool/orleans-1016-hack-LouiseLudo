c1c045be125dc5de559a73ddd0726bd03d534fec
louisemunier <munier.louise0@gmail.com>
2016-10-06 19:06:23 +0200

    19h06

--- afficherMusic.php
+++ afficherMusic.php
@@ -1,16 +1,17 @@
 <?php
 
-$json_string = file_get_contents('https://api.deezer.com/playlist/1911334042/tracks', true);
-$tract = json_decode($json_string);
+//$json_string = file_get_contents('https://api.deezer.com/playlist/1911334042/tracks', true);
+//$tract = json_decode($json_string);
 
 //var_dump($tract);
 
-
+//echo htmlspecialchars($_POST)
 
 
 
 //GET
 
+//https://api.deezer.com/search?title=%cloud%
 
 
 
