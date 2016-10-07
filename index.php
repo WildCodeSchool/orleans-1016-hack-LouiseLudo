42f93ef76d266acc8ef6c94bace8325a28a51c0a
ludovicmoreau <ludovic_moreau@laposte.net>
2016-10-07 02:54:48 +0200

    Sauvegarde body

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
     <div class="container-fluid">
         <div class="jumbotron">
-            <h1>WEATHERMOOD</h1>
+            <h1 id="titre">WEATHERMOOD</h1>
             <div class="row-formulaire">
                 <span style="font-family: 'Dosis', sans-serif;">
                 <form  method = "GET" action="index.php" class="col-lg-offset-3 col-lg-9">
@@ -27,10 +27,14 @@
         </div>
         <div class="row">
             <div class=" text1 col-lg-6">
-                AEIO
+                <h2>Music</h2>
+                <img class="musiquedessin img-responsive" src="http://www.gif-maniac.com/gifs/51/50514.gif" alt="musique">
+                <p>Music is always connected to the weather</p>
             </div>
             <div class=" text2 col-lg-6">
-                YOP
+                <h2>Weather in the World </h2>
+                <img class="sundessin img-responsive" src="http://www.icone-gif.com/gif/espace/soleil/1reflect.gif" alt="sun">
+                <p>Find the weather in the world in one click!</p>
             </div>
         </div>
 
